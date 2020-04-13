739f72c8e29e582479b2dd52e2f943b505b15f40
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-12 22:49:33 -0400

    copy css styling from previous tweeter project

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ function App() {
     <div className="App">
       <Navigation />
       <Profile />
-      <TweetForm />
-      <Tweet />
+      <main class="container">
+        <TweetForm />
+        <Tweet />
+      </main>
     </div>
   );
 }
